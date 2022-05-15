434a44f2b401f66d4de27e47bb01cf6d5c5091b7
yunus <altinisikyunusss@gmail.com>
2022-05-15 11:00:29 +0200

    the last one

--- game.py
+++ game.py
@@ -1,30 +1,83 @@
-import time
 import json
+from user import users
+i=1
+import time
 import datetime
-
-class game:
-    def __init__(self,user):
-        self.converted_time_total=user.TIME
-        self.starting_time()
-
+class game_play():
+    
+    def __init__(self):
+        self.timer=0
+        
+    
     def starting_time(self):
         self.startingtime=time.time()
 
     def ending_time(self):
         self.endingtime=time.time()
+        game_play.total_time()
 
     def total_time(self):
         self.totaltime=round(self.startingtime-self.endingtime)
         self.converted_time=datetime.timedelta(seconds=self.totaltime)
         return self.converted_time
-
+    
+    def show_word(self,level):
+        self.nl=[]
+        self.en=[]
+        self.level=level
+        with open('word_list.json', 'r') as json_file:
+            data = json.load(json_file)
+            i=0
+            for j in data[str(self.level)]:
+                if i<20:
+                    self.nl.append(j)
+                    self.en.append(data[str(self.level)][j])
+                    i=i+1
+        return self.nl, self.en
 
     
-    def show_word(self):
-        
-        with open('word_list.json', 'r') as json_file:
-          data = json.load(json_file)
-          level=self.user_level
-          for j in data[level]:
-              print("h:",j)
-              print("e",data[level][j])
+    
+    def true(self,obj):
+        global i
+        self.obj=obj
+        if i==20:
+            self.obj.level=self.obj.level+1
+            obj.register(self.obj.level,555)
+            i=0
+        self.word_nl,self.word_en=game_play().show_word(self.obj.level)
+        self.nl=self.word_nl[i]
+        self.en=self.word_en[i]
+        i=i+1
+        # total_seconds = 3
+        # while total_seconds >= 0:
+        #     timer = datetime.timedelta(seconds = total_seconds)
+        #     print(timer, end="\r")
+        #     time.sleep(1)
+        #     total_seconds -= 1
+        return self.nl, self.en, i-1
+            
+    
+    # def false(self):
+    #     global i
+    #     self.false_nl=[]
+    #     self.false_en=[]
+    #     self.level=1
+    #     if i==20:
+    #         level=level+1
+    #         i=0
+    #     self.word_nl,self.word_en=game_play().show_word(self.level)
+    #     self.nl=self.word_nl[i]
+    #     self.en=self.word_en[i]
+    #     self.false_nl=self.nl
+    #     self.false_en=self.false_en
+    #     i=i+1
+    #     # total_seconds = 3
+    #     # while total_seconds >= 0:
+    #     #     timer = datetime.timedelta(seconds = total_seconds)
+    #     #     print(timer, end="\r")
+    #     #     time.sleep(1)
+    #     #     total_seconds -= 1
+    #     return self.nl, self.en
+
+
+
--- login.py
+++ login.py
@@ -2,6 +2,8 @@ from PyQt5 import QtWidgets,uic
 import menu
 from logo import logo_rc
 import aboutus 
+import user
+
 
 
 class login_window(QtWidgets.QMainWindow):
@@ -9,12 +11,21 @@ class login_window(QtWidgets.QMainWindow):
     def __init__(self):
         super(login_window, self).__init__()
         uic.loadUi('ui/login.ui', self)
-        self.enter_button.clicked.connect(self.menu_show)
+        self.enter_button.clicked.connect(self.create)
         self.about_us_button.clicked.connect(self.aboutus_show)
         self.show()
         
-    def menu_show(self):
-        self.cams=menu.menu_window()
+    def create(self):
+        a=user.users(self.username_line.text())
+        a.search() 
+        self.menu_show(a)
+        
+    def menu_show(self,obj):
+        self.obj=obj
+        self.cams=menu.menu_window(self.obj)
+        self.cams.welcome_label.setText("WELCOME "+str(self.obj.name))
+        self.cams.total_time_label.setText("TOTAL TIME "+str(self.obj.totalTime))
+        self.cams.level_label.setText("LEVEL "+str(self.obj.level))
         self.cams.show()
         self.close()
         
